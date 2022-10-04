98300edd3b0fc752efc8f7df6309f61ba5d1e1d4
alirehmanarshad <98810654+ansmalik446@users.noreply.github.com>
2022-10-04 11:52:53 +0500

    cvxfvxc

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -16,16 +16,7 @@
             For any enquiries, complete the below contact form or email us at hello@wealth-wheel.com.
           </p>
           <div class="d-flex ww-banner-btn">
-          @guest
-          @if (Route::has('login')) 
-          <a href="{{ route ('register') }}" class="btn-get-started animate__animated animate__fadeInUp scrollto">Sign Up</a>
-          @endif
-          @if (Route::has('login'))
-          <a href="{{ route ('login') }}" class="btn-get-started animate__animated animate__fadeInUp scrollto">Login</a>
-          @endif
-          @else
-          <a href="{{ route ('logout') }}" class="btn-get-started animate__animated animate__fadeInUp scrollto">Logout</a>
-          @endguest
+         
           <a href="{{ route ('wealth_wheel') }}" class="btn-get-started animate__animated animate__fadeInUp scrollto">Create Wealth Wheel</a>
           </div>
         </div>
